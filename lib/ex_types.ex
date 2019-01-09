53c751eee8b388ab705b036a5b693a371a430ef9
Michael Davis <mcd164@miami.edu>
2019-01-08 22:38:55 -0500

    Initial commit

---
+++ lib/ex_types.ex
@@ -0,0 +1,18 @@
+defmodule ExTypes do
+  @moduledoc """
+  Documentation for ExTypes.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTypes.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_types_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTypesTest do
+  use ExUnit.Case
+  doctest ExTypes
+
+  test "greets the world" do
+    assert ExTypes.hello() == :world
+  end
+end
